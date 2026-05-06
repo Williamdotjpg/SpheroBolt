c66fb0fabfabf865bebd5d7192adbb18d39935e0
CyleKrohling <cylekrohling10@gmail.com>
2026-05-06 13:44:04 -0400

    Added Hello World - Stable

--- Code/SpheroBoltCode.js
+++ Code/SpheroBoltCode.js
@@ -5,5 +5,11 @@
 
 
 async function startProgram() {
-	// Write code here
+    await speak("Hello World", true);
+    setMainLed({ r: 0, g: 0, b: 255 });
+    setSpeed(60);
+    await delay(2);
+    setSpeed(0);
+
 }
+
