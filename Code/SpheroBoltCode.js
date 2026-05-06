8549de0ade6f2f6c25d0f3cebd9898eaa87b0760
CyleKrohling <cylekrohling10@gmail.com>
2026-05-06 14:12:51 -0400

    Hello square

--- Code/SpheroBoltCode.js
+++ Code/SpheroBoltCode.js
@@ -4,11 +4,33 @@
 //Program : Sandbox Training
 
 
-async function startProgram() {
+/*async function startProgram() {
     await speak("goodbye world", true);
     setMainLed({ r: 200, g: 0, b: 255 });
     setSpeed(70);
     await delay(3);
     setSpeed(0);
+
+}
+*/
+async function startProgram() {
+
+    setMainLed({ r: 0, g: 0, b: 255 });
+
+    await speak("Hello Square", true);
+
+    await delay(1);
+
+    for (var _i1 = 0; _i1 < 4; _i1++) {
+
+        setMainLed(getRandomColor());
+
+        await Sound.Game.Coin.play(true);
+
+        await roll((getHeading() + 90), 60, 2);
+
+        await delay(1);
+
+    }
 
 }
